9e31e0a327813ef99084ebaf0d007965467dd8c1
frankda <36907108+frankda@users.noreply.github.com>
2020-04-19 19:45:29 +1000

    Updates

---
+++ precache-manifest.6c5ce729c55b9c5c152c08d0a89b9c9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858f62448bfc9cdc61af482f040c5c0f",
+    "url": "//frankda.github.io/color-selector/index.html"
+  },
+  {
+    "revision": "9410e6756fdb598e73e4",
+    "url": "//frankda.github.io/color-selector/static/css/main.5f971f9f.chunk.css"
+  },
+  {
+    "revision": "3e7917186926a1668d6d",
+    "url": "//frankda.github.io/color-selector/static/js/2.54a04833.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//frankda.github.io/color-selector/static/js/2.54a04833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9410e6756fdb598e73e4",
+    "url": "//frankda.github.io/color-selector/static/js/main.77f7eb4b.chunk.js"
+  },
+  {
+    "revision": "9bfd070fbf1068224344",
+    "url": "//frankda.github.io/color-selector/static/js/runtime-main.4a880bc1.js"
+  }
+]);
